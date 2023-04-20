8876f22ce77739af7feaba73ab65befeffd7a027
Rayyan Alfawazy Adheswarna <adheswarna@gmail.com>
2023-04-20 10:12:35 +0700

    front and back initial spawn

--- Assets/Scripts/PlayManager.cs
+++ Assets/Scripts/PlayManager.cs
@@ -8,16 +8,26 @@ public class PlayManager : MonoBehaviour
     [SerializeField] Road roadPrefab;
     [SerializeField] int initialGrassCount = 5;
     [SerializeField] int horizontalSize;
-    [SerializeField] int backRelativePos = -5; // Spawn Line ke Belakang
+    [SerializeField] int backViewDistance = -5; // Spawn Line ke Belakang
+    [SerializeField] int frontViewDistance = 7; // Spawn Line ke Depan (Relative terhadap player)
 
     private void Start()
     {
-        for (int zPos = backRelativePos; zPos < initialGrassCount; ++zPos) 
+        // Membuat Initial Grass
+        for (int zPos = backViewDistance; zPos < initialGrassCount; ++zPos) 
         {
             var grass = Instantiate(grassPrefab);
             grass.transform.localPosition = new Vector3(0,0,zPos);
             grass.SetTreePercentage(zPos < -1 ? 1 : 0); //Jika zPos dibawah 0 maka spawn semua pohon.
             grass.Generate(horizontalSize);
+        }
+
+        // Membuat Line Kedepan Player
+        for (int zPos = initialGrassCount; zPos < frontViewDistance; ++zPos)
+        {
+            var terrain = Instantiate(roadPrefab);
+            terrain.transform.localPosition = new Vector3(0, 0, zPos);
+            terrain.Generate(horizontalSize);
         }
     }
 }
